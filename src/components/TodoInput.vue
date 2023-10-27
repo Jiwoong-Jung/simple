256fc6ac0d2f9268c46fbdb3f3db8762742764c4
Jiwoong-Jung <jjw981119@naver.com>
2023-10-27 16:12:29 +0900

    버튼 이벤트 추가

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,12 +1,26 @@
-<script setup>
-export default {
+<template>
+  <div>
+    <input type="text" v-model="newTodoItem">
+    <button v-on:click="addTodo">추가</button>
+  </div>
+</template>
 
+<script>
+export default {
+  data() {
+    return {
+      newTodoItem: ''
+    }
+  },
+  methods: {
+    addTodo() {
+      console.log(this.newTodoItem);
+    }
+  }
 }
 </script>
 
-<template>
-<div>input</div>
-</template>
+
 
 <style scoped>
 
